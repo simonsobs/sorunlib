c33f8d2268cb2190953f42fd786c8f36d78d5818
Brian Koopman <brian.koopman@yale.edu>
2021-12-14 16:44:59 -0500

    Organize commands into a class

--- src/sorunlib/commands.py
+++ src/sorunlib/commands.py
@@ -1,38 +1,36 @@
 from .util import create_clients
 
-CLIENTS = create_clients()
 
+class ObservationRunner:
+    def __init__(self):
+        self.clients = create_clients()
 
-def move_to(az, el, wait=1):
-    """Move telescope to specified coordinates.
+    def move_to(self, az, el, wait=1):
+        """Move telescope to specified coordinates.
 
-    """
-    CLIENTS['acu'].go_to(az, el)
+        """
+        self.clients['acu'].go_to(az, el)
 
+    def start_scan(self):
+        pass
 
-def start_scan():
-    pass
+    def bias_step(self):
+        """Perform a bias step on all SMuRF Controllers"""
+        for smurf in self.clients['smurf']:
+            smurf.run.start('bias_step.sh')
 
+        # group wait - does this work?
+        for smurf in self.clients['smurf']:
+            smurf.run.wait()
 
-def bias_step():
-    """Perform a bias step on all SMuRF Controllers"""
-    for smurf in CLIENTS['smurf']:
-        smurf.run.start('bias_step.sh')
+    def iv_curve(self):
+        """Perform a bias step on all SMuRF Controllers"""
+        for smurf in self.clients['smurf']:
+            smurf.run.start('iv_curve.sh')
 
-    # group wait - does this work?
-    for smurf in CLIENTS['smurf']:
-        smurf.run.wait()
+        # group wait - does this work?
+        for smurf in self.clients['smurf']:
+            smurf.run.wait()
 
-
-def iv_curve():
-    """Perform a bias step on all SMuRF Controllers"""
-    for smurf in CLIENTS['smurf']:
-        smurf.run.start('iv_curve.sh')
-
-    # group wait - does this work?
-    for smurf in CLIENTS['smurf']:
-        smurf.run.wait()
-
-
-def wait():
-    pass
+    def wait(self):
+        pass
