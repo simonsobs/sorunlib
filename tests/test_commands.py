259aa5bdf1374da72af426ca18980043a0324f34
Brian Koopman <brian.koopman@yale.edu>
2021-12-14 23:00:47 -0500

    Write tests for ObservationRunner class methods

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,21 +1,50 @@
-from sorunlib import commands
+import os
+os.environ["OCS_CONFIG_DIR"] = "./test_util/"
+
+from unittest.mock import MagicMock, patch
+
+from sorunlib.commands import ObservationRunner
 
 
+def mocked_clients():
+    clients = {'acu': MagicMock(),
+               'smurf': [MagicMock(), MagicMock(), MagicMock()]}
+
+    return clients
+
+
+@patch('sorunlib.commands.create_clients', mocked_clients)
 def test_move_to():
-    commands.move_to()
+    run = ObservationRunner()
+    run.move_to(180, 60)
+    run.clients['acu'].go_to.assert_called_with(180, 60)
 
 
+@patch('sorunlib.commands.create_clients', mocked_clients)
 def test_start_scan():
-    commands.start_scan()
+    run = ObservationRunner()
+    run.start_scan()
 
 
+@patch('sorunlib.commands.create_clients', mocked_clients)
 def test_bias_step():
-    commands.bias_step()
+    run = ObservationRunner()
+    run.bias_step()
+    for client in run.clients['smurf']:
+        client.run.start.assert_called_with('bias_step.sh')
+        client.run.start.assert_called_once()
 
 
+@patch('sorunlib.commands.create_clients', mocked_clients)
 def test_iv_curve():
-    commands.iv_curve()
+    run = ObservationRunner()
+    run.iv_curve()
+    for client in run.clients['smurf']:
+        client.run.start.assert_called_with('iv_curve.sh')
+        client.run.start.assert_called_once()
 
 
+@patch('sorunlib.commands.create_clients', mocked_clients)
 def test_wait():
-    commands.wait()
+    run = ObservationRunner()
+    run.wait()
