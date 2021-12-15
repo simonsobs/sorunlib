18e05590f8001acd73e5d24fc3ed7a4d7d901a47
Brian Koopman <brian.koopman@yale.edu>
2021-12-15 12:05:27 -0500

    Add docstrings to classes and methods

--- src/sorunlib/commands.py
+++ src/sorunlib/commands.py
@@ -5,14 +5,29 @@ from .util import create_clients
 
 
 class ObservationRunner:
+    """An object for running observations. It gathers the required clients
+    for commanding the needed hardware, and provides methods for
+    orchestrating actions across groups of Agents.
+
+    Attributes:
+        clients (dict): A dictionary of clients needed for orchestrating
+            observations. See :func:`sorunlib.util.create_clients` for
+            structure.
+
+    """
     def __init__(self):
         self.clients = create_clients()
 
-    def move_to(self, az, el, wait=1):
+    def move_to(self, az, el, wait=None):
         """Move telescope to specified coordinates.
 
+        Args:
+            az (float): destination angle for the azimuthal axis
+            el (float): destination angle for the elevation axis
+            wait (float): amount of time to wait for motion to end
+
         """
-        self.clients['acu'].go_to(az, el)
+        self.clients['acu'].go_to(az, el, wait)
 
     def start_scan(self):
         pass
@@ -22,7 +37,6 @@ class ObservationRunner:
         for smurf in self.clients['smurf']:
             smurf.run.start('bias_step.sh')
 
-        # group wait - does this work?
         for smurf in self.clients['smurf']:
             smurf.run.wait()
 
@@ -31,7 +45,6 @@ class ObservationRunner:
         for smurf in self.clients['smurf']:
             smurf.run.start('iv_curve.sh')
 
-        # group wait - does this work?
         for smurf in self.clients['smurf']:
             smurf.run.wait()
 
