28d12c070744c4acc91967084087ca82a2fd0934
Brian Koopman <brian.koopman@yale.edu>
2021-12-15 11:10:59 -0500

    Organize ObservationRunner tests into a class

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -15,48 +15,39 @@ def mocked_clients():
     return clients
 
 
-@patch('sorunlib.commands.create_clients', mocked_clients)
-def test_move_to():
-    run = ObservationRunner()
-    run.move_to(180, 60)
-    run.clients['acu'].go_to.assert_called_with(180, 60)
+class TestObservationRunner:
+    @pytest.fixture
+    @patch('sorunlib.commands.create_clients', mocked_clients)
+    def run(self):
+        """Setup an ObservationRunner with mocked Clients."""
+        run = ObservationRunner()
+        return run
 
+    def test_move_to(self, run):
+        run.move_to(180, 60)
+        run.clients['acu'].go_to.assert_called_with(180, 60)
 
-@patch('sorunlib.commands.create_clients', mocked_clients)
-def test_start_scan():
-    run = ObservationRunner()
-    run.start_scan()
+    def test_start_scan(self, run):
+        run.start_scan()
 
+    def test_bias_step(self, run):
+        run.bias_step()
+        for client in run.clients['smurf']:
+            client.run.start.assert_called_with('bias_step.sh')
+            client.run.start.assert_called_once()
 
-@patch('sorunlib.commands.create_clients', mocked_clients)
-def test_bias_step():
-    run = ObservationRunner()
-    run.bias_step()
-    for client in run.clients['smurf']:
-        client.run.start.assert_called_with('bias_step.sh')
-        client.run.start.assert_called_once()
+    def test_iv_curve(self, run):
+        run.iv_curve()
+        for client in run.clients['smurf']:
+            client.run.start.assert_called_with('iv_curve.sh')
+            client.run.start.assert_called_once()
 
+    def test_wait_in_past(self, run):
+        with pytest.raises(AssertionError):
+            run.wait("2020-01-01T00:00:00")
 
-@patch('sorunlib.commands.create_clients', mocked_clients)
-def test_iv_curve():
-    run = ObservationRunner()
-    run.iv_curve()
-    for client in run.clients['smurf']:
-        client.run.start.assert_called_with('iv_curve.sh')
-        client.run.start.assert_called_once()
-
-
-@patch('sorunlib.commands.create_clients', mocked_clients)
-def test_wait_in_past():
-    run = ObservationRunner()
-    with pytest.raises(AssertionError):
-        run.wait("2020-01-01T00:00:00")
-
-
-# patch out time.sleep so we don't actually wait during testing
-@patch('sorunlib.commands.time.sleep', MagicMock())
-@patch('sorunlib.commands.create_clients', mocked_clients)
-def test_wait():
-    run = ObservationRunner()
-    target = dt.datetime.now() + dt.timedelta(seconds=1)
-    run.wait(target.isoformat())
+    # patch out time.sleep so we don't actually wait during testing
+    @patch('sorunlib.commands.time.sleep', MagicMock())
+    def test_wait(self, run):
+        target = dt.datetime.now() + dt.timedelta(seconds=1)
+        run.wait(target.isoformat())
