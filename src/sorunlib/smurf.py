390281d33e9f3615a7c9d44150572331b16c43e8
Brian Koopman <brian.koopman@yale.edu>
2022-06-16 18:54:31 +0000

    Add remaining smurf task and process wrappers

--- src/sorunlib/smurf.py
+++ src/sorunlib/smurf.py
@@ -11,9 +11,42 @@ def bias_step():
 
 
 def iv_curve():
-    """Perform a bias step on all SMuRF Controllers"""
+    """Perform an iv curve on all SMuRF Controllers"""
     for smurf in run.CLIENTS['smurf']:
         smurf.take_iv.start()
 
     for smurf in run.CLIENTS['smurf']:
         smurf.take_iv.wait()
+
+
+def tune_dets(test_mode=False):
+    """Perform detector tuning on all SMuRF Controllers.
+
+    Args:
+        test_mode (bool): Run tune_dets() task in test_mode, removing emulated
+            wait times.
+
+    """
+    for smurf in run.CLIENTS['smurf']:
+        smurf.tune_dets.start(test_mode=test_mode)
+
+    for smurf in run.CLIENTS['smurf']:
+        smurf.tune_dets.wait()
+
+
+def bias_dets():
+    """Bias the detectors on all SMuRF Controllers"""
+    for smurf in run.CLIENTS['smurf']:
+        smurf.bias_dets.start()
+
+    for smurf in run.CLIENTS['smurf']:
+        smurf.bias_dets.wait()
+
+
+def stream():
+    """Stream data on all SMuRF Controllers"""
+    for smurf in run.CLIENTS['smurf']:
+        smurf.stream.start()
+
+    for smurf in run.CLIENTS['smurf']:
+        print(smurf.stream.status())
