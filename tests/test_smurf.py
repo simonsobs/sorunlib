00665419c956311f0df230a528758cd00508b3ed
Brian Koopman <brian.koopman@yale.edu>
2022-06-16 18:54:31 +0000

    Add remaining smurf task and process wrappers

--- tests/test_smurf.py
+++ tests/test_smurf.py
@@ -27,3 +27,27 @@ def test_iv_curve():
     smurf.iv_curve()
     for client in smurf.run.CLIENTS['smurf']:
         client.take_iv.start.assert_called_once()
+
+
+@patch('sorunlib.create_clients', mocked_clients)
+def test_tune_dets():
+    smurf.run.initialize(test_mode=True)
+    smurf.tune_dets(test_mode=True)
+    for client in smurf.run.CLIENTS['smurf']:
+        client.tune_dets.start.assert_called_once()
+
+
+@patch('sorunlib.create_clients', mocked_clients)
+def test_bias_dets():
+    smurf.run.initialize(test_mode=True)
+    smurf.bias_dets()
+    for client in smurf.run.CLIENTS['smurf']:
+        client.bias_dets.start.assert_called_once()
+
+
+@patch('sorunlib.create_clients', mocked_clients)
+def test_stream():
+    smurf.run.initialize(test_mode=True)
+    smurf.stream()
+    for client in smurf.run.CLIENTS['smurf']:
+        client.stream.start.assert_called_once()
