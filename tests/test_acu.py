87059355c667d40060b56e6523b241178d19d5a0
Brian Koopman <brian.koopman@yale.edu>
2022-06-16 16:16:26 +0000

    Handle task failure in acu.move_to()

--- tests/test_acu.py
+++ tests/test_acu.py
@@ -1,8 +1,11 @@
 import os
 os.environ["OCS_CONFIG_DIR"] = "./test_util/"
 
+import pytest
+
 from unittest.mock import MagicMock, patch
 
+from ocs.ocs_client import OCSReply
 from sorunlib import acu
 
 
@@ -18,3 +21,12 @@ def test_move_to():
     acu.run.initialize()
     acu.move_to(180, 60)
     acu.run.CLIENTS['acu'].go_to.assert_called_with(az=180, el=60, wait=None)
+
+
+@patch('sorunlib.create_clients', mocked_clients)
+def test_move_to_failed():
+    acu.run.initialize()
+    mocked_response = OCSReply(0, 'msg', {'success': False})
+    acu.run.CLIENTS['acu'].go_to.side_effect = [mocked_response]
+    with pytest.raises(RuntimeError):
+        acu.move_to(180, 90)
