7441c2537f1320667cc51d55cbdb926fe467fc7e
Brian Koopman <brian.koopman@yale.edu>
2023-08-03 16:36:33 -0400

    Add boresight rotation to move_to()

--- src/sorunlib/acu.py
+++ src/sorunlib/acu.py
@@ -2,13 +2,32 @@ import sorunlib as run
 from sorunlib._internal import check_response
 
 
-def move_to(az, el):
+def move_to(az, el, boresight=None):
     """Move telescope to specified coordinates.
 
     Args:
         az (float): destination angle for the azimuthal axis
         el (float): destination angle for the elevation axis
+        boresight (float, optional): destination angle for boresight rotation
+            on SAT platforms
+
+    Raises:
+        RuntimeError: If boresight is passed to a non-satp platform.
 
     """
+    # Az/El motion
     resp = run.CLIENTS['acu'].go_to(az=az, el=el)
     check_response(resp)
+
+    # Boresight motion (satp only)
+    if boresight is None:
+        return
+
+    # Check platform type
+    resp = run.CLIENTS['acu'].monitor.status()
+    platform = resp.session['data']['PlatformType']
+    if platform == "satp":
+        resp = run.CLIENTS['acu'].set_boresight(target=boresight)
+        check_response(resp)
+    else:
+        raise RuntimeError(f"Platform type {platform} does not support boresight motion")
---
+++ tests/util.py
@@ -0,0 +1,11 @@
+from unittest import mock
+
+from ocs.ocs_agent import OpSession
+
+
+def create_session(op_name):
+    """Create an OpSession with a mocked app for testing."""
+    mock_app = mock.MagicMock()
+    session = OpSession(1, op_name, app=mock_app)
+
+    return session
