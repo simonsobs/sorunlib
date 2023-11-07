ea61e61d041effc6c315578f7ec59ea6d1e8f9b0
Brian Koopman <brian.koopman@yale.edu>
2023-11-07 18:35:46 -0500

    Create check_running internal method

--- src/sorunlib/_internal.py
+++ src/sorunlib/_internal.py
@@ -7,6 +7,21 @@ The usual caveats apply, these interfaces might change without notice.
 import ocs
 
 
+def _check_error(client, response):
+    """Check if a response is an error or timeout."""
+    op = response.session['op_name']
+    instance = client.instance_id
+
+    if response.status == ocs.ERROR:
+        error = f"Request for Operation {op} in Agent {instance} failed.\n" + \
+            str(response)
+        raise RuntimeError(error)
+    elif response.status == ocs.TIMEOUT:
+        error = f"Timeout reached waiting for {op} in Agent {instance} to " + \
+            "complete.\n" + str(response)
+        raise RuntimeError(error)
+
+
 def check_response(client, response):
     """Check that a response from OCS indicates successful task/process
     completion.
@@ -25,16 +40,33 @@ def check_response(client, response):
     op = response.session['op_name']
     instance = client.instance_id
 
-    if response.status == ocs.ERROR:
-        error = f"Request for Operation {op} in Agent {instance} failed.\n" + \
-            str(response)
-        raise RuntimeError(error)
-    elif response.status == ocs.TIMEOUT:
-        error = f"Timeout reached waiting for {op} in Agent {instance} to " + \
-            "complete.\n" + str(response)
-        raise RuntimeError(error)
+    _check_error(client, response)
 
     if not response.session['success']:
         error = f'Task {op} in Agent {instance} failed to complete " + \
             "successfully.\n' + str(response)
+        raise RuntimeError(error)
+
+
+def check_running(client, response):
+    """Check that a response from OCS indicates a task/process is still
+    running.
+
+    Args:
+        client (ocs.ocs_client.OCSClient): OCS Client which returned the
+            response.
+        response (ocs.ocs_client.OCSReply): Response from an OCS operation
+            call.
+
+    Raises:
+        RuntimeError: When Operation is not running.
+
+    """
+    op = response.session['op_name']
+    instance = client.instance_id
+
+    _check_error(client, response)
+    if response.session['status'] != 'running':
+        error = f"Operation {op} in Agent {instance} is not in the 'running' " + \
+                "state.\n" + str(response)
         raise RuntimeError(error)
--- tests/test__internal.py
+++ tests/test__internal.py
@@ -6,16 +6,18 @@ from unittest.mock import MagicMock
 from ocs.ocs_agent import OpSession
 from ocs.ocs_client import OCSReply
 
-from sorunlib._internal import check_response
+from sorunlib._internal import check_response, check_running
 
 os.environ["OCS_CONFIG_DIR"] = "./test_util/"
 
 
-def create_session(op_name, success=None):
+def create_session(op_name, status=None, success=None):
     """Create an OpSession with a mocked app for testing."""
     mock_app = MagicMock()
     session = OpSession(1, op_name, app=mock_app)
     session.op_name = 'test_op'
+    if status is not None:
+        session.set_status(status)
     session.success = success
 
     return session.encoded()
@@ -32,10 +34,16 @@ invalid_responses = [(MockClient(), OCSReply(ocs.TIMEOUT,
                                              create_session('test', success=True))),
                      (MockClient(), OCSReply(ocs.ERROR,
                                              'msg',
-                                             create_session('test')))]
+                                             create_session('test', success=False)))]
 
 valid_responses = [
     (MockClient(), OCSReply(ocs.OK, 'msg', create_session('test', success=True)))]
+
+invalid_running_responses = [
+    (MockClient(), OCSReply(ocs.OK, 'msg', create_session('test')))]
+
+running_responses = [
+    (MockClient(), OCSReply(ocs.OK, 'msg', create_session('test', status='running')))]
 
 
 @pytest.mark.parametrize("client,response", invalid_responses)
@@ -47,3 +55,14 @@ def test_check_response_raises(client, response):
 @pytest.mark.parametrize("client,response", valid_responses)
 def test_check_response(client, response):
     check_response(client, response)
+
+
+@pytest.mark.parametrize("client,response", invalid_running_responses)
+def test_check_running_raises(client, response):
+    with pytest.raises(RuntimeError):
+        check_running(client, response)
+
+
+@pytest.mark.parametrize("client,response", running_responses)
+def test_check_running(client, response):
+    check_running(client, response)
