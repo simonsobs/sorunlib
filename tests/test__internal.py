aa7f5685a418bff6d50e96c5be8e77c265d5e792
Brian Koopman <brian.koopman@yale.edu>
2023-12-13 16:53:22 -0500

    Create check_started function

--- tests/test__internal.py
+++ tests/test__internal.py
@@ -2,9 +2,10 @@ import os
 import ocs
 import pytest
 
+from unittest.mock import MagicMock, patch
 from ocs.ocs_client import OCSReply
 
-from sorunlib._internal import check_response, check_running
+from sorunlib._internal import check_response, check_running, check_started
 
 from util import create_session as create_unencoded_session
 
@@ -20,6 +21,7 @@ def create_session(*args, **kwargs):
 class MockClient:
     def __init__(self):
         self.instance_id = 'test-id'
+        self.test_op = MagicMock()
 
 
 invalid_responses = [(MockClient(), OCSReply(ocs.TIMEOUT,
@@ -33,10 +35,13 @@ valid_responses = [
     (MockClient(), OCSReply(ocs.OK, 'msg', create_session('test', success=True)))]
 
 invalid_running_responses = [
-    (MockClient(), OCSReply(ocs.OK, 'msg', create_session('test')))]
+    (MockClient(), OCSReply(ocs.OK, 'msg', create_session('test', status='done')))]
 
 running_responses = [
     (MockClient(), OCSReply(ocs.OK, 'msg', create_session('test', status='running')))]
+
+invalid_starting_responses = [
+    (MockClient(), OCSReply(ocs.OK, 'msg', create_session('test', status='starting')))]
 
 
 @pytest.mark.parametrize("client,response", invalid_responses)
@@ -59,3 +64,21 @@ def test_check_running_raises(client, response):
 @pytest.mark.parametrize("client,response", running_responses)
 def test_check_running(client, response):
     check_running(client, response)
+
+
+@patch('sorunlib._internal.time.sleep', MagicMock())
+@pytest.mark.parametrize("client,response", invalid_starting_responses)
+def test_check_started_raises_timeout(client, response):
+    with pytest.raises(RuntimeError):
+        check_started(client, response)
+
+
+@pytest.mark.parametrize("client,response", invalid_running_responses)
+def test_check_started_raises(client, response):
+    with pytest.raises(RuntimeError):
+        check_started(client, response, timeout=1)
+
+
+@pytest.mark.parametrize("client,response", running_responses)
+def test_check_started(client, response):
+    check_started(client, response)
