976643354f14c2628ea4e7bfb89c6b4d2c2a6746
Brian Koopman <brian.koopman@yale.edu>
2024-03-08 17:29:25 -0500

    Create HWP module API

---
+++ src/sorunlib/hwp.py
@@ -0,0 +1,26 @@
+# Public API
+def set_freq(freq):
+    """Set the rotational frequency of the HWP.
+
+    Args:
+        freq (float): Target frequency to rotate the HWP in Hz. This is a
+            *signed float*, the meaning of which depends on the OCS site
+            configuration. For details see the `documentation for the HWP
+            Supervisor Agent <docs_>`_.
+
+    .. _docs: https://socs.readthedocs.io/en/main/agents/hwp_supervisor_agent.html
+
+    """
+    pass
+
+
+def stop(active=True):
+    """Stop the HWP.
+
+    Args:
+        active (bool, optional): If True, actively try to stop the HWP by
+            applying the brake. If False, simply turn off the PMX power and let
+            it spin down on its own. Defaults to True.
+
+    """
+    pass
