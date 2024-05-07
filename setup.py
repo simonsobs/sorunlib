d8f19da479f5c5c262f5daa6334c0839deba1b54
Brian Koopman <brian.koopman@yale.edu>
2024-05-07 17:35:13 -0400

    Drop Python 3.7 and add 3.11 and 3.12 to support list

--- setup.py
+++ setup.py
@@ -23,15 +23,16 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: BSD License",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Astronomy",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=[
         'ocs==0.11.0',
         'pyyaml',
