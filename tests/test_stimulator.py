897bf4df8e117a949d904951819c0256b58200f7
dixilo <dixilo.kimus@gmail.com>
2025-06-06 17:39:54 +0900

    fix:enhance coverage of the test

--- tests/test_stimulator.py
+++ tests/test_stimulator.py
@@ -4,6 +4,7 @@ os.environ["OCS_CONFIG_DIR"] = "./test_util/"
 import pytest
 from unittest.mock import MagicMock, patch
 
+from ocs.ocs_client import OCSReply
 from sorunlib import stimulator
 
 from util import create_patch_clients
@@ -13,8 +14,16 @@ patch_clients_lat = create_patch_clients('lat')
 
 
 @pytest.mark.parametrize("do_setup", [True, False])
+@pytest.mark.parametrize("smurf_error", [True, False])
 @patch('sorunlib.stimulator.time.sleep', MagicMock())
-def test_calibrate_tau(patch_clients_lat, do_setup):
+def test_calibrate_tau(patch_clients_lat, do_setup, smurf_error):
+    if smurf_error:
+        mocked_response = OCSReply(
+            0, 'msg', {'success': False, 'op_name': 'stream'})
+        stimulator.run.CLIENTS['smurf'][0].stream.wait.side_effect = [mocked_response]
+        stimulator.run.CLIENTS['smurf'][1].stream.wait.side_effect = [mocked_response]
+        stimulator.run.CLIENTS['smurf'][2].stream.wait.side_effect = [mocked_response]
+
     stimulator.calibrate_tau(do_setup=do_setup)
 
     # setup test
@@ -35,9 +44,16 @@ def test_calibrate_tau(patch_clients_lat, do_setup):
 
 
 @pytest.mark.parametrize("do_setup", [True, False])
+@pytest.mark.parametrize("smurf_error", [True, False])
 @patch('sorunlib.stimulator.time.sleep', MagicMock())
-def test_calibrate_gain(patch_clients_lat, do_setup):
+def test_calibrate_gain(patch_clients_lat, do_setup, smurf_error):
     # setup test
+    if smurf_error:
+        mocked_response = OCSReply(
+            0, 'msg', {'success': False, 'op_name': 'stream'})
+        stimulator.run.CLIENTS['smurf'][0].stream.wait.side_effect = [mocked_response]
+        stimulator.run.CLIENTS['smurf'][1].stream.wait.side_effect = [mocked_response]
+        stimulator.run.CLIENTS['smurf'][2].stream.wait.side_effect = [mocked_response]
     stimulator.calibrate_gain(do_setup=do_setup)
 
     if do_setup:
@@ -52,4 +68,3 @@ def test_calibrate_gain(patch_clients_lat, do_setup):
     # stop test
     stimulator.run.CLIENTS['stimulator']['blh'].stop_rotation.assert_called_with()
     stimulator.run.CLIENTS['stimulator']['ds378'].set_relay.assert_any_call(relay_number=1, on_off=0)
-    
