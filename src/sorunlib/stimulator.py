145022b0ee06e0fd4f9ba6f3ca949e84db88a332
YudaiSeino <seino.yudai.53d@st.kyoto-u.ac.jp>
2025-09-24 18:46:30 -0400

    Fix typo

--- src/sorunlib/stimulator.py
+++ src/sorunlib/stimulator.py
@@ -86,7 +86,7 @@ def calibrate_tau(duration_step=20,
         else:
             if filter_cutoff is None:
                 filter_cutoff = int(63/200 * 4000/downsample_factor)
-            tag += f', filter_cutoff {filter_cutoff:.0f}',
+            tag += f', filter_cutoff {filter_cutoff:.0f}'
 
             if filter_order is not None and filter_order != 4:
                 tag += f', filter_order {filter_order:.0f}'
@@ -177,7 +177,7 @@ def calibrate_gain(duration=60, speed_rpm=90,
         else:
             if filter_cutoff is None:
                 filter_cutoff = int(63/200 * 4000/downsample_factor)
-            tag += f', filter_cutoff {filter_cutoff:.0f}',
+            tag += f', filter_cutoff {filter_cutoff:.0f}'
             if filter_order is not None and filter_order != 4:
                 tag += f', filter_order {filter_order:.0f}'
 
